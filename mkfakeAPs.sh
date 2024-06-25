ca6985fc2368fbcb61689d2505c53ba67277a74b
pinscher666 <willy.peterson.mesquita@gmail.com>
2024-06-25 18:05:34 -0300

    Add files via upload

---
+++ mkfakeAPs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [ $1 == "-h" ]; then
+   echo -e "*CREATE FAKE APs*\n  Git:pinscher666\n\n  usage: nmkfakeAPs <option> <[1~10]> <param>\n\n  Options:\n  -i <count>                 :input manual names\n  -r <count> <name>          :repeat the several names"
+   exit 0
+fi
+
+if [ $1 != "-r" ] && [ $1 != "-i" ] || [ $2 -gt 10 ] || [ $2 -lt 1 ]; then
+   echo -e "invalid argument \n use: mkfakeAPs -h"
+   exit 0
+fi
+
+echo -n > listfakeAPs.txt
+
+for((mac=1;mac<=$2;mac++));
+do
+  if((mac<10)); then
+     echo -n "00:00:00:00:00:0$mac " >> listfakeAPs.txt
+  elif((mac>9)); then
+     echo -n "00:00:00:00:00:$mac " >> listfakeAPs.txt
+  fi
+  if [ $1 == "-i" ]; then
+     echo -e "name($mac):"
+     read _INPUT_STRING
+     echo "$_INPUT_STRING" >> listfakeAPs.txt
+  elif [ $1 == "-r" ]; then
+     echo "$3$mac" >> listfakeAPs.txt
+  fi
+done
+
+echo "create list sucess"
